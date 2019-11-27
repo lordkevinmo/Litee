02193dbbc236d69ff69c577f9d595184af6017aa
Moise AGBENYA <moise.agbenya@gmail.com>
2019-11-27 16:59:29 +0100

    updating parser

---
+++ src/com/gama/litee/Parser.java
@@ -0,0 +1,47 @@
+package com.gama.litee;
+
+import java.util.List;
+
+import static com.gama.litee.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr comparison() {
+    }
+
+    private Token previous() {
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
